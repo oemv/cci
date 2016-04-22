a2e17629ba53d706d3602235a7f7d612aed78b04
OEMV <ovelazqu@ms.ds.uhc.com>
2016-04-22 16:18:36 -0700

    Added bit-vector based solution for Q4

--- src/main/java/com/oemv/cci/ch1/Q4Temp.java
+++ src/main/java/com/oemv/cci/ch1/Q4Temp.java
@@ -2,6 +2,7 @@ package com.oemv.cci.ch1;
 
 public class Q4Temp {
 
+	// Solution 2: O(N) --> keeping track of oddCounts as the phrase is iterated
 	public static boolean isPermutationOfPalindrome(String phrase) {
 		int[] counts = new int[27];
 		int oddCount = 0;
@@ -21,7 +22,7 @@ public class Q4Temp {
 
 	/**
 	 * Maps a character to a number of 1 to 26 (case-insensitive). <br />
-	 * a->1, b->2, C->3, D->4
+	 * a->1, b->2, C->3, D->4, ...
 	 * 
 	 * @param _char
 	 * @return
@@ -30,11 +31,45 @@ public class Q4Temp {
 		int a = Character.getNumericValue('a');
 		int z = Character.getNumericValue('z');
 		int $char = Character.getNumericValue(_char);
+		return ($char <= z && $char >= a) ? $char - a + 1 : -1;
+	}
 
-		if ($char <= z && $char >= a) {
-			return $char - a + 1;
-		} else {
-			return -1;
+	// Solution 3: O(N) Optimizing space using a bit-vector
+	public static boolean isPermutationOfPalindrome2(String phrase) {
+		int vector = createVector(phrase);
+		return (vector == 0) || isSingleBitOn(vector);
+	}
+
+	/**
+	 * Takes a string and generates an bit vector that represents the parity of
+	 * the letters
+	 * 
+	 * @param phrase
+	 * @return
+	 */
+	public static int createVector(String phrase) {
+		int vector = 0;
+		for (char c : phrase.toCharArray()) {
+			if (!Character.isWhitespace(c)) {
+				vector = toggle(vector, c);
+			}
 		}
+		return vector;
+	}
+
+	/**
+	 * Toggles a bit in a bit vector
+	 * 
+	 * @param vector
+	 * @param c
+	 * @return
+	 */
+	public static int toggle(int vector, char c) {
+		int mask = 1 << map(c);
+		return vector ^= mask;
+	}
+
+	public static boolean isSingleBitOn(int vector) {
+		return ((vector - 1) & vector) == 0;
 	}
 }
--- src/test/java/com/oemv/cci/ch1/Q4TestTemp.java
+++ src/test/java/com/oemv/cci/ch1/Q4TestTemp.java
@@ -16,6 +16,14 @@ public class Q4TestTemp {
 	}
 
 	@Test
+	public void testToggleBit() {
+		int vector = 0;
+		int toggled = Q4Temp.toggle(vector, 'c');
+		Assert.assertEquals("1000", Integer.toBinaryString(toggled));
+		Assert.assertTrue(0 == Q4Temp.toggle(toggled, 'c'));
+	}
+
+	@Test
 	public void testIsPermutationOfPalindrome() {
 		Assert.assertTrue(Q4Temp.isPermutationOfPalindrome("Coa Coa"));
 		Assert.assertTrue(Q4Temp.isPermutationOfPalindrome("x"));
@@ -28,6 +36,21 @@ public class Q4TestTemp {
 		Assert.assertFalse(Q4Temp.isPermutationOfPalindrome("Tacos"));
 		Assert.assertFalse(Q4Temp.isPermutationOfPalindrome("Godzilla"));
 		Assert.assertFalse(Q4Temp.isPermutationOfPalindrome("Banana"));
+	}
+
+	@Test
+	public void testIsPermutationOfPalindrome2() {
+		Assert.assertTrue(Q4Temp.isPermutationOfPalindrome2("Coa Coa"));
+		Assert.assertTrue(Q4Temp.isPermutationOfPalindrome2("x"));
+		Assert.assertTrue(Q4Temp.isPermutationOfPalindrome2("xx"));
+		Assert.assertTrue(Q4Temp.isPermutationOfPalindrome2("Anita lava LA tina"));
+	}
+
+	@Test
+	public void testIsNotPermutationOfPalindrome2() {
+		Assert.assertFalse(Q4Temp.isPermutationOfPalindrome2("Tacos"));
+		Assert.assertFalse(Q4Temp.isPermutationOfPalindrome2("Godzilla"));
+		Assert.assertFalse(Q4Temp.isPermutationOfPalindrome2("Banana"));
 	}
 
 }
