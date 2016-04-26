cad16ea8047d45f912b7e4b11ab795bc1169d25e
OEMV <oscar1_@hotmail.com>
2016-04-25 21:53:33 -0700

    Fixed Q7

--- src/main/java/com/oemv/cci/ch1/Q7.java
+++ src/main/java/com/oemv/cci/ch1/Q7.java
@@ -2,17 +2,49 @@ package com.oemv.cci.ch1;
 
 public class Q7 {
 
+	// O(N^2)
 	public static int[][] rotate(int[][] m) {
 		int length = m[0].length;
 		for (int i = 0; i < length / 2; i++) {
-	
+			int max = length - i - 1;
+			for (int j = 0; j < max - i; j++) {
+				int temp = m[i][i + j];
+				m[i][i + j] = m[max - j][i];
+				m[max - j][i] = m[max][max - j];
+				m[max][max - j] = m[i + j][max];
+				m[i + j][max] = temp;
+			}
 		}
 		return m;
 	}
 
-	public static void main(String[] args) {
-		print(rotate(new int[][] { { 1, 2 }, { 3, 4 } }));
-		print(rotate(rotate(new int[][] { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } })));
+	// Book's solution
+	public static boolean rotate2(int[][] matrix) {
+		if (matrix.length == 0 || matrix.length != matrix[0].length)
+			return false; // Not a square
+		int n = matrix.length;
+
+		for (int layer = 0; layer < n / 2; layer++) {
+			int first = layer;
+			int last = n - 1 - layer;
+			for (int i = first; i < last; i++) {
+				int offset = i - first;
+				int top = matrix[first][i]; // save top
+
+				// left -> top
+				matrix[first][i] = matrix[last - offset][first];
+
+				// bottom -> left
+				matrix[last - offset][first] = matrix[last][last - offset];
+
+				// right -> bottom
+				matrix[last][last - offset] = matrix[i][last];
+
+				// top -> right
+				matrix[i][last] = top; // right <- saved top
+			}
+		}
+		return true;
 	}
 
 	public static void print(int[][] matrix) {
@@ -22,7 +54,5 @@ public class Q7 {
 			}
 			System.out.println();
 		}
-
 	}
-
 }
--- src/test/java/com/oemv/cci/ch1/Q7Test.java
+++ src/test/java/com/oemv/cci/ch1/Q7Test.java
@@ -5,15 +5,13 @@ import org.junit.Ignore;
 import org.junit.Test;
 
 public class Q7Test {
-
-	@Ignore
+	
 	@Test
 	public void testRotate1by1() {
 		int[][] src = new int[][] { { 1 } };
 		Assert.assertArrayEquals(src, Q7.rotate(src));
 	}
-
-	@Ignore
+	
 	@Test
 	public void testRotate2by2() {
 		int[][] src = new int[][] { { 1, 2 }, 
@@ -23,7 +21,6 @@ public class Q7Test {
 		Assert.assertArrayEquals(expected, Q7.rotate(src));
 	}
 
-	@Ignore
 	@Test
 	public void testRotate3by3() {
 		int[][] src = new int[][] { { 1, 2, 3 }, 
@@ -49,6 +46,23 @@ public class Q7Test {
 									     { 15, 11, 7, 3 }, 
 									     { 16, 12, 8, 4 } };
 		
+		Assert.assertArrayEquals(expected, Q7.rotate(src));
+	}
+	
+	@Test
+	public void testRotate5by5() {
+		int[][] src = new int[][] { { 1,   2,  3,  4 ,  5}, 
+									{ 6,   7,  8,  9 , 10}, 
+									{ 11, 12, 13, 14 , 15}, 
+									{ 16, 17, 18, 19 , 20}, 
+									{ 21, 22, 23, 24 , 25}};
+									
+		int[][] expected= new int[][] { { 21, 16, 11,  6 , 1}, 
+										{ 22, 17, 12,  7 , 2}, 
+										{ 23, 18, 13,  8 , 3}, 
+										{ 24, 19, 14,  9 , 4}, 
+										{ 25, 20, 15, 10 , 5}};
+
 		Assert.assertArrayEquals(expected, Q7.rotate(src));
 	}
 
