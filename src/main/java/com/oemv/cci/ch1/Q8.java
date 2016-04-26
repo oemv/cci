79f9d79103e384237a41cbf0331b004da609ce16
OEMV <ovelazqu@ms.ds.uhc.com>
2016-04-26 11:39:48 -0700

    Added book solution for Q8

--- src/main/java/com/oemv/cci/ch1/Q8.java
+++ src/main/java/com/oemv/cci/ch1/Q8.java
@@ -17,7 +17,7 @@ public class Q8 {
 		for (int i = 0; i < M; i++) {
 			for (int j = 0; j < N; j++) {
 				if (1 == registry[i][j]) {
-					m = _zerofy(m, i, j);
+					_zerofy(m, i, j);
 				}
 			}
 		}
@@ -25,7 +25,7 @@ public class Q8 {
 		return m;
 	}
 
-	private static int[][] _zerofy(int[][] m, int row, int column) {
+	private static void _zerofy(int[][] m, int row, int column) {
 		// zerofy row
 		for (int i = 0; i < m[0].length; i++) {
 			m[row][i] = 0;
@@ -34,12 +34,11 @@ public class Q8 {
 		for (int i = 0; i < m.length; i++) {
 			m[i][column] = 0;
 		}
-		return m;
 	}
 
-	
 	/**
 	 * Zerofy marking rows and columns
+	 * 
 	 * @param m
 	 * @return
 	 */
@@ -61,30 +60,90 @@ public class Q8 {
 
 		for (int i = 0; i < M; i++) {
 			if (1 == rows[i]) {
-				m = zerofyRow(m, i);
+				zerofyRow(m, i);
 			}
 		}
 
 		for (int i = 0; i < N; i++) {
 			if (1 == columns[i]) {
-				m = zerofyColumn(m, i);
+				zerofyColumn(m, i);
 			}
 		}
-
 		return m;
 	}
 
-	private static int[][] zerofyColumn(int[][] m, int column) {
+	private static void zerofyColumn(int[][] m, int column) {
 		for (int i = 0; i < m.length; i++) {
 			m[i][column] = 0;
 		}
-		return m;
 	}
 
-	private static int[][] zerofyRow(int[][] m, int row) {
+	private static void zerofyRow(int[][] m, int row) {
 		for (int i = 0; i < m[0].length; i++) {
 			m[row][i] = 0;
 		}
-		return m;
-	}
+	}	
+	
+	/**
+	 * From Book: First row and column are traversed to check if they need to be nullified,
+	 *  then the rest of the matrix is traversed and indications to either nullify or not the row/column are stored 
+	 *  in the first row/column itself.
+	 * @param matrix
+	 */
+	public static int[][] setZeros(int[][] matrix) {
+		boolean rowHasZero = false;
+		boolean colHasZero = false;		
+		
+		// Check if first row has a zero
+		for (int j = 0; j < matrix[0].length; j++) {
+			if (matrix[0][j] == 0) {
+				rowHasZero = true;
+				break;
+			}
+		}		
+		
+		// Check if first column has a zero
+		for (int i = 0; i < matrix.length; i++) {
+			if (matrix[i][0] == 0) {
+				colHasZero = true;
+				break;
+			}
+		}		
+		
+		// Check for zeros in the rest of the array
+		for (int i = 1; i < matrix.length; i++) {
+			for (int j = 1; j < matrix[0].length;j++) {
+				if (matrix[i][j] == 0) {
+					matrix[i][0] = 0;
+					matrix[0][j] = 0;
+		 		}
+			}
+		}		
+		
+		// Nullify rows based on values in first column
+		for (int i = 1; i < matrix.length; i++) {
+			if (matrix[i][0] == 0) {
+				zerofyRow(matrix, i);
+			}
+		}		
+		
+		// Nullify columns based on values in first row
+		for (int j = 1; j < matrix[0].length; j++) {
+			if (matrix[0][j] == 0) {
+				zerofyColumn(matrix, j);
+			}
+		}	
+		
+		// Nullify first row
+		if (rowHasZero) {
+			zerofyRow(matrix, 0);
+		}
+		
+		// Nullify first column
+		if (colHasZero) {
+			zerofyColumn(matrix, 0);
+		}
+		
+		return matrix;
+	}	
 }
--- src/test/java/com/oemv/cci/ch1/Q8Test.java
+++ src/test/java/com/oemv/cci/ch1/Q8Test.java
@@ -6,7 +6,7 @@ import org.junit.Test;
 public class Q8Test {
 
 	@Test
-	public void testZerofy1by1() {
+	public void testZerofy1by1_1() {
 		int[][] src = new int[][] { { 0 } };
 		Assert.assertArrayEquals(src, Q8.zerofy(src));
 		
@@ -15,7 +15,7 @@ public class Q8Test {
 	}
 	
 	@Test
-	public void testZerofy2by2() {
+	public void testZerofy2by2_1() {
 		int[][] src = new int[][] {{ 1, 2 }, 
 								   { 3, 4 }};
 		Assert.assertArrayEquals(src, Q8.zerofy(src));
@@ -28,7 +28,7 @@ public class Q8Test {
 	}
 
 	@Test
-	public void testZerofy3by3() {
+	public void testZerofy3by3_1() {
 		int[][] src = new int[][] {{ 1, 2, 3 }, 
 								   { 4, 5, 6 }, 
 								   { 7, 8, 9 } };
@@ -46,7 +46,7 @@ public class Q8Test {
 	}
 
 	@Test
-	public void testZerofy4by4() {
+	public void testZerofy4by4_1() {
 		int[][] src = new int[][] {{ 1,   2,  3,  4 }, 
 								   { 5,   6,  7,  8 }, 
 								   { 9,  10, 11, 12 }, 
@@ -69,7 +69,7 @@ public class Q8Test {
 	}
 	
 	@Test
-	public void testZerofy5by5() {
+	public void testZerofy5by5_1() {
 		int[][] src = new int[][] {{ 1,   2,  3,  4 ,  5}, 
 								   { 6,   7,  8,  9 , 10}, 
 								   { 11, 12, 13,  14, 15}, 
@@ -94,7 +94,7 @@ public class Q8Test {
 	}
 	
 	@Test 
-	public void testZerofy2by6(){
+	public void testZerofy2by6_1(){
 		int[][] src = new int[][]{{1,2,3, 4, 5, 6},
 								  {7,8,9,10,11,12}};
 								  
@@ -212,4 +212,108 @@ public class Q8Test {
         				               {7,8,9,10, 0,12}};
         Assert.assertArrayEquals(expected, Q8.zerofy2(src));								  
 	}
+	
+	// book
+		@Test
+		public void testZerofy1by1_3() {
+			int[][] src = new int[][] { { 0 } };
+			Assert.assertArrayEquals(src, Q8.setZeros(src));
+			
+			 src = new int[][] { { 1 } };
+			Assert.assertArrayEquals(src, Q8.setZeros(src));
+		}
+		
+		@Test
+		public void testZerofy2by2_3() {
+			int[][] src = new int[][] {{ 1, 2 }, 
+									   { 3, 4 }};
+			Assert.assertArrayEquals(src, Q8.setZeros(src));
+			
+			src = new int[][] {{ 1, 0 }, 
+							   { 3, 4 }};
+			int[][] expected = new int[][] {{ 0, 0 }, 
+									 		{ 3, 0 }};
+			Assert.assertArrayEquals(expected, Q8.setZeros(src));
+		}
+
+		@Test
+		public void testZerofy3by3_3() {
+			int[][] src = new int[][] {{ 1, 2, 3 }, 
+									   { 4, 5, 6 }, 
+									   { 7, 8, 9 } };
+			Assert.assertArrayEquals(src, Q8.setZeros(src));
+			
+			src = new int[][] {{ 1, 2, 3 }, 
+							   { 4, 0, 6 }, 
+							   { 7, 8, 9 }};
+				
+			int[][] expected = new int[][] {{ 1, 0, 3 }, 
+				     				        { 0, 0, 0 }, 
+				     				        { 7, 0, 9 }};
+		    Assert.assertArrayEquals(expected, Q8.setZeros(src));
+
+		}
+
+		@Test
+		public void testZerofy4by4_3() {
+			int[][] src = new int[][] {{ 1,   2,  3,  4 }, 
+									   { 5,   6,  7,  8 }, 
+									   { 9,  10, 11, 12 }, 
+									   { 13, 14, 15, 16 }};
+			
+			Assert.assertArrayEquals(src, Q8.setZeros(src));
+			
+			
+			src = new int[][] {{ 1,   2,  3,  4 }, 
+							   { 5,   0,  7,  8 }, 
+							   { 9,   0, 11, 12 }, 
+							   { 13, 14, 15, 16 }};
+				
+			int [][] expected = new int[][] {{  1, 0,  3,  4 }, 
+				     				 		 {  0, 0,  0,  0 }, 
+				     				 		 {  0, 0,  0,  0 }, 
+				     				 		 { 13, 0, 15, 16 }};
+
+		     Assert.assertArrayEquals(expected, Q8.setZeros(src));
+		}
+		
+		@Test
+		public void testZerofy5by5_3() {
+			int[][] src = new int[][] {{ 1,   2,  3,  4 ,  5}, 
+									   { 6,   7,  8,  9 , 10}, 
+									   { 11, 12, 13,  14, 15}, 
+									   { 16, 17, 18, 19 , 20}, 
+									   { 21, 22, 23, 24 , 25}};
+
+			Assert.assertArrayEquals(src, Q8.setZeros(src));
+			
+			src = new int[][] {{  0,  2,  3,  4 ,  5}, 
+				   			   {  6,  7,  8,  9 , 10}, 
+				   			   { 11, 12,  0,  14, 15}, 
+				   			   { 16, 17, 18, 19 , 20}, 
+				   			   { 21, 22, 23, 24 ,  0}};
+			
+			int[][] expected = new int[][] {{ 0,  0, 0,  0 , 0}, 
+										    { 0,  7, 0,  9 , 0}, 
+											{ 0,  0, 0,  0 , 0}, 
+											{ 0, 17, 0, 19 , 0}, 
+											{ 0,  0, 0,  0 , 0}};
+
+			Assert.assertArrayEquals(expected, Q8.setZeros(src));
+		}
+		
+		@Test 
+		public void testZerofy2by6_3(){
+			int[][] src = new int[][]{{1,2,3, 4, 5, 6},
+									  {7,8,9,10,11,12}};
+									  
+			Assert.assertArrayEquals(src, Q8.setZeros(src));
+			
+			src = new int[][]{{1,2,3, 4, 0, 6},
+				              {7,8,9,10,11,12}};
+				              
+	        int[][] expected = new int[][]{{0,0,0, 0, 0, 0},
+	        				               {7,8,9,10, 0,12}};
+	        Assert.assertArrayEquals(expected, Q8.setZeros(src));								  
+		}
 }
